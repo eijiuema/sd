e77947c56a0c1247fe99e66020920021b44f814f
Eiji Uema <eijiuema@gmail.com>
2019-09-17 23:57:27 -0300

    .

--- atividade_2/atividade2.py
+++ atividade_2/atividade2.py
@@ -1,97 +1,64 @@
-import os
 import sys
+from time import sleep
 import socket
 import pickle
-from time import sleep
-from queue import Queue
 from threading import Thread
 
-PROCCESS_N = 3
-HOST = '127.0.0.1'
+ADDRESS = '127.0.0.1'
 PORT = 5000
-
-queues = [Queue()]*PROCCESS_N
-clients = [None]*PROCCESS_N
 
 def main():
 
-    for i in range(0, 3):
-        Thread(target=t_process, args=[i, queues[i]], daemon=True).start()
+    pid = int(sys.argv[1])
+
+    process = Process(pid)
 
     while True:
-        id, tempo = input().split()
-        id = int(id)
-        tempo = int(tempo)
-        if 0 <= id < PROCCESS_N:
-            queues[id].put(tempo)
-
-def t_process(id, queue):
-
-    log_file = open(f'log_{id}.txt', 'w')
-
-    clock = 0
-    # 0 => Não requisitado
-    # 1 => Requisitado
-    # 2 => Em uso
-    recurso = 0
-
-    def log(message):
-        log_file.write(f'{clock}: {message}\n')
-        log_file.flush()
-        os.fsync(log_file.fileno())
-
-    log(f'Iniciando processo com id = {id}')
-
-    def t_listen():
-        nonlocal clock
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((HOST, PORT + id))
-        server.listen(5)
-        log(f'Ouvindo na porta = {PORT + id}')
-        clients[id] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clients[id].connect((HOST, PORT + id))
-        while True:
-            client, addr = server.accept()
-            log('Cliente conectado')
-            while True:
-                data = pickle.loads(client.recv(1024))
-
-                if data['clock'][0] > clock:
-                    clock = data['clock'][0]
-
-                clock += 1
-
-                if data['message'] == 'request':
-
-                    log(f'Enviado OK ao processo {data["clock"][1]}')
-                    if recurso == 0:
-                        data = {
-                            'clock': (clock, id),
-                            'message': 'ok'
-                        }
-                        client.send(pickle.dumps(data))
-
-                if data['message'] == 'ok':
-                    
-
-                log(f'Dados recebidos: {data}')
-
-    Thread(target=t_listen, daemon=True).start()
-
-    while True:
-        if not queue.empty():
-            tempo = queue.get()
-            clock += 1
-            for client in clients:
-                data = {
-                    'clock': (clock, id),
-                    'message': 'request'
-                }
-                client.send(pickle.dumps(data))
+        duracao = int(input())
+        process.send(0, duracao)
         sleep(1)
 
-if __name__ == '__main__':
-    try:
-        main()
-    except (KeyboardInterrupt):
-        pass
+class Process():
+    OK = 0
+    REQUEST = 1
+    USING = 2
+
+    def __init__(self, pid):
+        self.pid = pid
+        self.clock = 0
+        self.state = Process.OK
+        Thread(target=self.t_listen, daemon=True).start()
+
+    def t_listen(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((ADDRESS, PORT+self.pid))
+        server.listen(5)
+        print(f"Listening on port {PORT+self.pid}")
+        while True:
+            client, addr = server.accept()
+            print(f"Connected from {addr}")
+            data = pickle.loads(client.recv(1024))
+
+            self.clock = max(self.clock, data['clock'])
+
+            
+
+            print(data)
+            client.close()
+
+
+    def send(self, target_pid, message):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((ADDRESS, PORT+self.pid))
+        
+        data = {
+            'pid': self.pid,
+            'clock': self.clock,
+            'message': message
+        }
+
+        client.send(pickle.dumps(data))
+
+if __name__ == "__main__":
+    main()
+    pass
---
+++ atividade_2/test.bat
@@ -0,0 +1,3 @@
+start py atividade2.py 0
+start py atividade2.py 1
+start py atividade2.py 2
